e86da85deae4fdc4bbe4e5d649367840f328838b
Mike Reich <mjreich@gmail.com>
2016-02-08 19:56:15 -0800

    Added additional tests

--- test/index.js
+++ test/index.js
@@ -16,6 +16,7 @@ describe("Router", () => {
  
   beforeEach(() => {
     app = new TestApp();
+    router = new Router(app);
   });
   
   describe("Load", () => {
@@ -25,5 +26,64 @@ describe("Router", () => {
       router = new Router(app);
       router.should.not.be.null;
     });
+  });
+
+  describe("Init", () => {
+    it("should register for app lifecycle", () => {
+      app.once.called.should.be.true;
+      app.once.calledWith('load').should.be.true;
+      app.once.calledWith('launch').should.be.true;
+      app.once.calledWith('stop').should.be.true;
+    });
+
+    it("should have port after load", () => {
+      return app.emit('load').then(() => {
+        router.should.have.property('port');
+        router.should.have.property('routeTable');
+      });
+    });
+
+    it("should register a gather for routes", () => {
+      return app.emit('load').then(() => {
+        app.get.calledWith('router').should.be.true;
+        app.get().gather.calledWith('route').should.be.true;
+      });
+    })
+
+    it("should register a handler for getExpressApp", () => {
+      return app.emit('load').then(() => {
+        app.get().respond.calledWith('getExpressApp').should.be.true;
+      });
+    })
+
+    it("should register a handler for setAssets", () => {
+      return app.emit('load').then(() => {
+        app.get().respond.calledWith('setAssets').should.be.true;
+      });
+    })
+
+    it("should register a handler for setStatic", () => {
+      return app.emit('load').then(() => {
+        app.get().respond.calledWith('setStatic').should.be.true;
+      });
+    })
+
+    it("should register a handler for setRoute", () => {
+      return app.emit('load').then(() => {
+        app.get().respond.calledWith('setRoute').should.be.true;
+      });
+    })
+
+    it("should register a handler for setRoute.get", () => {
+      return app.emit('load').then(() => {
+        app.get().respond.calledWith('setRoute.get').should.be.true;
+      });
+    })
+
+    it("should register a handler for setRoute.post", () => {
+      return app.emit('load').then(() => {
+        app.get().respond.calledWith('setRoute.post').should.be.true;
+      });
+    })    
   });
 });
